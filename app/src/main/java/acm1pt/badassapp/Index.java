0d5c51a77cbf5789e5baee926c6aadf993d9b2a7
Fer <fernandojos44@gmail.com>
2016-04-03 17:37:33 -0500

    First Contact w/Android
    One small step for man, one giant leap for mankind - John Cena

---
+++ app/src/main/java/acm1pt/badassapp/Index.java
@@ -0,0 +1,13 @@
+package acm1pt.badassapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Index extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index);
+    }
+}
