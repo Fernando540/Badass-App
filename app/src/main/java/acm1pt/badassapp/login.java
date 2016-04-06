e8f8091324e9e3768af9adafbeb4363ff93d6d97
kikerique <enrique-romero_98@hotmail.com>
2016-04-05 20:17:31 -0500

    Código para consumir web service disque agregado :v

--- app/src/main/java/acm1pt/badassapp/login.java
+++ app/src/main/java/acm1pt/badassapp/login.java
@@ -7,6 +7,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
+
 import java.sql.SQLOutput;
 
 public class login extends AppCompatActivity implements View.OnClickListener{
@@ -17,7 +22,7 @@ public class login extends AppCompatActivity implements View.OnClickListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login1);
+        setContentView(R.layout.activity_login);
         correin=(EditText)findViewById(R.id.correo);
         pass=(EditText)findViewById(R.id.password);
         entrar=(Button) findViewById(R.id.btn);
@@ -27,6 +32,55 @@ public class login extends AppCompatActivity implements View.OnClickListener{
     @Override
     public void onClick(View v) {
         if(v==entrar){
+            String NAMESPACE = "badasshouse.prograbatiz.com/WebServices/";
+            String URL = "badasshouse.prograbatiz.com/WebServices/WS_Login?wsdl";
+            String METHOD_NAME = "WS_Login";
+
+    /* Definir los parámetros del servicio web, que son los datos
+       que se extrajo de la descripción del servicio.
+    */
+            String SOAP_ACTION = "badasshouse.prograbatiz.com/WebServices/WS_Login";
+
+    /* Se crea el paquete SOAP a enviar con la petición al servicio web,
+       en este caso es el valor que se quiere convertir a grados Fahrenheit en este
+       ejemplo, de esto se encarga el método addProperty("<Propiedad>",<Valor>).
+       NOTA: A la propiedad envelope.dotNet se le da un valor de true debido a que
+       el servicio Web al cual nos queremos conectar esta codificado en .NET,
+       no siempre es así, esta información debería estar en el sitio oficial
+       que atiende este servicio web.
+    */
+
+            SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
+
+
+            request.addProperty(correin.getText().toString(), pass.getText().toString());
+            SoapSerializationEnvelope envelope =
+                    new SoapSerializationEnvelope(SoapEnvelope.VER11);
+            envelope.dotNet=true;
+            envelope.setOutputSoapObject(request);
+
+
+    /* Se captura la respuesta y se muestra en el TextView */
+            String respuesta;
+            HttpTransportSE androidHttpTransport = new HttpTransportSE(URL);
+            try {
+                androidHttpTransport.call(SOAP_ACTION, envelope);
+                SoapObject result=(SoapObject) envelope.getResponse();
+                respuesta = result.getProperty("resultado").toString();
+
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+
+            //Para cuando Sirva el WEB Service :v
+            /*if(respuesta.equals("invalido")){
+
+            }else{
+                Intent intent = new Intent(login.this,home.class);
+                startActivity(intent);
+                finish();
+            }*/
             if(correin.getText().toString().equals("e@e.com") && pass.getText().toString().equals("123")){
                 Intent intent = new Intent(login.this,home.class);
                 startActivity(intent);
